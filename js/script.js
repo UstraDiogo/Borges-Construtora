9b53c535528ea30cb67f25710437136fad594814
Diogo Moacir Ustra Pereira - Andrade Maia Advogados <diogo.pereira@andrademaia.com>
2025-10-14 19:18:22 -0300

    Atualizações footer css e html

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ function showSlides() {
     slideIndex = 1;
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 1000);
 }
